84e4e6d95b64f3aec0f9a16c8d0367d8a0d33c77
bdinh <baodinh96@gmail.com>
2019-01-10 18:08:37 -0800

    Added solutions for zip exercise

--- zip/zipserver/handlers/zips.go
+++ zip/zipserver/handlers/zips.go
@@ -1,1 +1,50 @@
 package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"path"
+	"strings"
+	"github.com/info441/exercises/zip/zipserver/models"
+)
+
+//ZipIndexHandler handles requests that should
+//return a slice of *Zip records for a given key
+type ZipIndexHandler struct {
+	index models.ZipIndex
+}
+
+//NewZipIndexHandler constructs a new ZipIndexHandler.
+//The `index` parameter will be used to get the zips
+//for the requested key.
+func NewZipIndexHandler(index models.ZipIndex) *ZipIndexHandler {
+	return &ZipIndexHandler{
+		index: index,
+	}
+}
+
+//ServeHTTP handles the HTTP requests
+func (zih *ZipIndexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//resource path will be like: /zips/city/seattle
+	//get the last part of the path and convert it
+	//to lower-case
+	key := path.Base(r.URL.Path)
+	key = strings.ToLower(key)
+	
+	//tell the client that the response body is JSON
+	//and allow cross-origin requests
+	w.Header().Add(headerContentType, contentTypeJSON)
+	w.Header().Add(headerAccessControlAllowOrigin, "*")
+	
+	//get the zips for the requested key
+	//and encode them into JSON
+	zips := zih.index[key]
+	json.NewEncoder(w).Encode(zips)
+}
+
+//RootHandler handles requests for the root resource
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add(headerAccessControlAllowOrigin, "*")
+	fmt.Fprintf(w, "Try requesting /zips/city/seattle")
+}
--- zip/zipserver/main.go
+++ zip/zipserver/main.go
@@ -1,5 +1,67 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"github.com/info441/exercises/zip/zipserver/handlers"
+	"github.com/info441/exercises/zip/zipserver/models"
 
+)
+
+func main() {
+	
+	//open the zips.csv file and report an errors
+	f, err := os.Open("./data/zips.csv")
+	if err != nil {
+		//if we can't open the file, we can't continue
+		//so use log.Fatalf() to report the error and
+		//stop the process
+		log.Fatalf("error opening zips.csv: %v", err)
+	}
+	
+	//load the zip code records from the file
+	//and report any errors
+	zips, err := models.LoadZips(f, 42613)
+	if err != nil {
+		//again, we can't continue in this case
+		//so use log.Fatal()
+		log.Fatalf("error parsing CSV: %v", err)
+	}
+	//close the file so we don't leave it open
+	//while the web zipserver is running
+	f.Close()
+	log.Printf("loaded %d zips", len(zips))
+	
+	//build a map index from city name (lower-cased)
+	//to a slice of *Zip for that city
+	cityIndex := models.ZipIndex{}
+	for _, z := range zips {
+		cityLower := strings.ToLower(z.City)
+		cityIndex[cityLower] = append(cityIndex[cityLower], z)
+	}
+	log.Printf("there are %d zips in Seattle", len(cityIndex["seattle"]))
+	
+	//construct a new ZipIndexHandler, passing the
+	//cityIndex as the index to use
+	cityZipHandler := handlers.NewZipIndexHandler(cityIndex)
+	
+	//get the address to listen on from the
+	//ADDR environment variable
+	addr := os.Getenv("ADDR")
+	if len(addr) == 0 {
+		addr = ":80"
+	}
+	
+	//create a new mux and add two routes:
+	// / => root handler
+	// /zips/city/<city-name> => cityZipHandler
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.RootHandler)
+	mux.Handle("/zips/city/", cityZipHandler)
+	
+	//start the web zipserver
+	log.Printf("zipserver is listening at https://%s", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
